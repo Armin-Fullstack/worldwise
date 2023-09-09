a3aeeeafff0390e80e53173469001ec770a056b8
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-09-09 14:43:58 +0330

    refactor: implemented a new approach for countries list Logic

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -11,17 +11,20 @@ export default function CountryList({ cities, isLoading }): JSX.Element {
       <Message message="Add your first city by clicking on a city on the map" />
     );
 
-  const countries = cities.reduce((array , city ) => {
-    if(!array.map(el => el.country).includes(city.country)){
-      return [...array , {country: city.country , emoji: city.emoji}]
-    } else return array
-  }
-  , [])
-
+  // const countries = cities.reduce((array , city ) => {
+  //   if(!array.map(el => el.country).includes(city.country)){
+  //     return [...array , {country: city.country , emoji: city.emoji}]
+  //   } else return array
+  // }
+  // , [])
+  const countries = Array.from(new Set(cities.map(city => city.country))).map(item => {
+    const city = cities.find(element => element.country === item)
+    return {country: city.country , emoji: city.emoji}
+  })
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} key={country.id} />
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
