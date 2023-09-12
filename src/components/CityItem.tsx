724cdec40f8c6248e1ae99e0f135601b1433865b
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-09-12 15:20:21 +0330

    feat: implemented Delete City functionality

--- src/components/CityItem.tsx
+++ src/components/CityItem.tsx
@@ -10,16 +10,21 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 export default function CityItem({ city }): JSX.Element {
-  const {currentCity} = useCities()
+  const {currentCity , deleteCity} = useCities()
   const { cityName, emoji, date, id , position: {lat , lng}} = city;
 
+
+  function handleClick(e) {
+    e.preventDefault()
+    deleteCity(id)
+  }
   return (
     <li>
       <Link className={`${styles.cityItem} ${city.id === currentCity.id ? styles["cityItem--active"] : ""}`} to={`${id}?lat=${lat}&lng=${lng}`}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>({formatDate(date)})</time>
-      <button className={styles.deleteBtn}>&times;</button>
+      <button className={styles.deleteBtn} onClick={handleClick}>&times;</button>
       </Link>
     </li>
   );
